353eaa751ac5d84f6bcec77f40db38a182df8094
unknown <rafagonpin317@gmail.com>
2024-04-29 23:37:50 -0300

    continuação homepage

--- app.py
+++ app.py
@@ -2,11 +2,7 @@ from flask import Flask , render_template
 
 app = Flask('__name__')
 
-<<<<<<< HEAD
 @app.route('/homepage')
-=======
-@app.route('/')
->>>>>>> b6604abfe3a85afa7c5fafc7a2dba4a53a740d3f
 def home():
     return render_template('index.html')
 
